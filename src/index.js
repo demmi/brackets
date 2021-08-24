452733ce73e357de798ad76e8bf4f873a1907b37
Andrew Gerasimchik <gerasimchick@tut.by>
2021-08-25 02:12:15 +0300

    create solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const brkt = {};
+
+  bracketsConfig.forEach((a) => (brkt[a[0]] = a[1]));
+
+  const arr = str.split("");
+
+  for (let i = 0; i < arr.length; i++) {
+      if (brkt[stack[stack.length - 1]] === arr[i]) {
+          stack.pop();
+      } else if (brkt.hasOwnProperty(arr[i])) {
+          stack.push(arr[i]);
+      } else {
+          return false;
+      }
+  }
+
+  return stack.length === 0 ? true : false;
 }
